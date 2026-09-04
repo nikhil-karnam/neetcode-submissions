842262c5bb33bc3f0611829ce300af41b7c3efad
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-04 17:21:41 -0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s == t;
+    }
+};
