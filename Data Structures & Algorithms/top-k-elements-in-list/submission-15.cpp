3d465226fc3e98c2ce0986ba70d9c61022997e1e
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-05 03:26:07 -0500

    Add: top-k-elements-in-list - submission-15

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-15.cpp
@@ -0,0 +1,32 @@
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> counts = {};
+        for(auto i : nums) {
+            counts[i]++;
+        }
+
+        //bucket sort O(n) for counts, but each count is paired with a vector of its associated nums
+        std::vector<std::vector<int>> sorted(nums.size() + 1);
+        for(auto i : counts){
+            sorted[i.second].push_back(i.first);
+        }
+
+        std::vector<int> res = {};
+        for(int i = nums.size(); i >= 0; i--){
+            if(k){
+                //if the count was not present, vector will be empty and loop will skip and k will not update.
+                for(auto j : sorted[i]){
+                    res.push_back(j);
+                }
+                k -= sorted[i].size();
+            }
+        }
+
+        return res;
+    }
+};
