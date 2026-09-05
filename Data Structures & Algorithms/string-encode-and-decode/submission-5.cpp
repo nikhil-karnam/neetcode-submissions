f83f99b5b86b1fb8e83319652303e62747d79d51
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-05 04:10:07 -0500

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        std::string s;
+        for(auto i : strs){
+            s += std::to_string(i.size()) + "#" + i;
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        std::vector<std::string> v;
+        for(int i = 0; i < s.size();){
+            std::string len;
+            for(; s[i] != '#'; i++){
+                len += s[i];
+            }
+            int len_int = std::stoi(len);
+            std::string curr;
+            //append i by one since we stopped at # and need to start scanning the actual string.
+            for(i++; len_int > 0; len_int--, i++){
+                curr += s[i];
+            }
+            v.push_back(curr);
+        }
+        return v;
+    }
+};
