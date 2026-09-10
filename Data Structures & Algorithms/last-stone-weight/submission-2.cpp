dbed59d70dc553e62bf650db9c496149df8afa67
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-10 01:45:42 -0500

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,30 @@
+#include <array>
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::array<int, 101> bucket{0};
+        for(auto i : stones){
+            bucket[i]++;
+        }
+
+        for(int i2 = 100, i1 = 99; i2 >= 0; i2--){
+            //set the new 2 indeces
+            for(; i2 >= 0 && bucket[i2] == 0; i2--);
+            for(i1 = i2 - 1; i1 >= 0 && bucket[i1] == 0; i1--);
+            //if i2 is the only stone left, it's value determines the result
+            if(i1 < 0){
+                if(bucket[i2] % 2){
+                    return i2;
+                }
+                return 0;
+            }
+            //otherwise, do the thing
+            if(bucket[i2] % 2){
+                bucket[i2-i1]++;
+                bucket[i1]--;
+            }
+        }
+        return 0;
+    }
+};
