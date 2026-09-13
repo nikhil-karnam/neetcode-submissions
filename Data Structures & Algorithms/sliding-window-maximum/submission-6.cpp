8c8ba66462d81ec1743a6a405b1fd08a86d219aa
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-13 01:53:31 -0500

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.cpp
@@ -0,0 +1,25 @@
+#include <deque>
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        std::deque<int> q;
+        std::vector<int> res;
+        for(int r = 0; r < nums.size(); r++){
+            while(!q.empty() && nums[r] >= nums[q.back()]){
+                q.pop_back();
+            }
+            q.push_back(r);
+            //always pushing back a valid index first, so no need !q.empty after
+            while(q.front() <=  r - k){
+                q.pop_front();
+            }
+            //first k-1 iterations are setup only
+            if(r >= k - 1){
+                res.push_back(nums[q.front()]);
+            }
+        }
+
+        return res;
+    }
+};
