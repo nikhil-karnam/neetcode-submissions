bc4d37eac9f0881ccfbae4dff4dc3ebee31002ec
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-14 00:18:48 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,36 @@
+class TimeMap {
+public:
+    struct Value{
+        std::string value;
+        int timestamp;
+    };
+    
+    std::unordered_map<std::string, std::vector<Value>> m;
+    
+    TimeMap() {
+
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        int low = 0;
+        int high = m[key].size() - 1;
+        while(low < high){
+            //reverse binary search
+            int mid = (low + high + 1) / 2;
+            if(m[key][mid].timestamp <= timestamp){
+                low = mid;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        if(m[key].size() == 0 || m[key][low].timestamp > timestamp){
+            return "";
+        }
+        return m[key][low].value;
+    }
+};
