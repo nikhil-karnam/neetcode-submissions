8ca28f7c868fad8d1f1efd5bea378709560c209c
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-14 00:33:46 -0500

    Add: find-median-in-a-data-stream - submission-3

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-3.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+#include <vector>
+
+class MedianFinder {
+public:
+    std::priority_queue<int> left;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> right;
+    
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        left.push(num);
+        right.push(left.top());
+        left.pop();
+        int mid = right.top();
+        right.pop();
+
+        if(left.size() == right.size()){
+            left.push(mid);
+        }
+        else{
+            right.push(mid);
+        }
+    }
+    
+    double findMedian() {
+        if(left.size() > right.size()){
+            return left.top();
+        }
+        //decimal to avoid truncation
+        return (left.top() + right.top()) / 2.0;
+    }
+};
