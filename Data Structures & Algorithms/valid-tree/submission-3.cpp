8b12aefc68effdcbbedbb13bf6a7d9536e32a2bc
Nikhil Karnam <44416779+nikhil-karnam@users.noreply.github.com>
2026-09-17 11:58:53 -0500

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        std::vector<std::vector<int>> dest(n);
+        for(auto& i : edges){
+            dest[i[0]].push_back(i[1]);
+            dest[i[1]].push_back(i[0]);
+        }
+
+        std::vector<bool> done(n, false);
+        std::queue<std::pair<int, int>> q;
+        q.push({0,0});
+        done[0] = true;
+        
+        while(!q.empty()){
+            int curr = q.front().first;
+            int origin = q.front().second;
+            for(auto& i : dest[curr]){
+                if(i != origin){
+                    if(done[i]){
+                        return false;
+                    }
+                    q.push({i, curr});
+                    done[i] = true;
+                }
+            }
+            q.pop();
+        }
+
+        return std::find(done.begin(), done.end(), 0) == done.end();
+    }
+};
